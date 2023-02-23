3f9b34e99cf1d278797b384d5c2ca1ad53fcc755
triciahughes <hughes.a.tricia@gmail.com>
2023-02-23 10:47:06 -0800

    completed lab

--- lib/book.py
+++ lib/book.py
@@ -1,4 +1,31 @@
 #!/usr/bin/env python3
 
 class Book:
+
+    def title(self):
+        return self.title
+
+    def __init__(self, title, page_count=0):
+        self.page_count = page_count
+        self.title = title
+
+    def author(self, author):
+        self.author = author
     pass
+
+    def get_page_count(self):
+        return self._page_count
+
+    def set_page_count(self, page_count):
+        if (type(page_count) == int):
+            self._page_count = page_count
+        else:
+            print("page_count must be an integer")
+
+
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
+
+# title = property(title,)
+    page_count = property(get_page_count, set_page_count,)
+    # turn_page = property(get_turn_page, turn_page)
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,26 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+
+    def get_brand(self):
+        return self._brand
+
+    def __init__(self, brand, size=0):
+        self.brand = brand
+        self.size = size
+
+    def set_size(self):
+        return self._size
+
+    def get_size(self, size):
+        if (type(size) == int):
+            self._size = size
+        else:
+            print("size must be an integer")
+
+    def cobble(self):
+        self.condition = "New"
+        print("Your shoe is as good as new!")
+   
+
+    size = property(set_size, get_size)
